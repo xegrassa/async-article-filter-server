20ea0e9c25fbb31520fd201c0be437ba0fa36e07
Egrassa <bacek90@mail.ru>
2022-02-27 17:23:37 +0500

    Тесты вынесены в отдельный каталог tests

--- core/text_tools.py
+++ core/text_tools.py
@@ -1,16 +1,6 @@
 import asyncio
 import string
 
-import pymorphy2
-import pytest
-
-
-def _clean_word(word):
-    word = word.replace('«', '').replace('»', '').replace('…', '')
-    # FIXME какие еще знаки пунктуации часто встречаются ?
-    word = word.strip(string.punctuation)
-    return word
-
 
 async def split_by_words(morph, text):
     """Учитывает знаки пунктуации, регистр и словоформы, выкидывает предлоги."""
@@ -22,26 +12,6 @@ async def split_by_words(morph, text):
             words.append(normalized_word)
         await asyncio.sleep(0)
     return words
-
-
-@pytest.mark.asyncio
-async def test_split_by_words():
-    # Экземпляры MorphAnalyzer занимают 10-15Мб RAM т.к. загружают в память много данных
-    # Старайтесь организовать свой код так, чтоб создавать экземпляр MorphAnalyzer заранее и в единственном числе
-    morph = pymorphy2.MorphAnalyzer()
-
-    assert await split_by_words(morph, 'Во-первых, он хочет, чтобы') == ['во-первых', 'хотеть', 'чтобы']
-
-    assert await split_by_words(morph, '«Удивительно, но это стало началом!»') == ['удивительно', 'это', 'стать',
-                                                                                   'начало']
-
-    # Эту часть удалить, как и большие файлы
-    with open('gogol_nikolay_taras_bulba_-_bookscafenet.txt', 'r', encoding='utf8') as f:
-        text_1 = f.read()
-    with open('gogol_after_split.txt', 'r', encoding='utf8') as f:
-        text_2 = f.readlines()
-    assert await split_by_words(morph, text_1) == list(map(str.rstrip, text_2))
-    ###################
 
 
 def calculate_jaundice_rate(article_words, charged_words):
@@ -57,10 +27,8 @@ def calculate_jaundice_rate(article_words, charged_words):
     return round(score, 2)
 
 
-def test_calculate_jaundice_rate():
-    assert -0.01 < calculate_jaundice_rate([], []) < 0.01
-    assert 33.0 < calculate_jaundice_rate(['все', 'аутсайдер', 'побег'], ['аутсайдер', 'банкротство']) < 34.0
-
-
-if __name__ == '__main__':
-    test_split_by_words()
+def _clean_word(word):
+    word = word.replace('«', '').replace('»', '').replace('…', '')
+    # FIXME какие еще знаки пунктуации часто встречаются ?
+    word = word.strip(string.punctuation)
+    return word
